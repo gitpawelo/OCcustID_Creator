aa5d666c928421d417158cb6b231e7c8cfc7bf3f
pawelpawel8787 <gitpawelo@wp.pl>
2019-05-27 23:06:21 +0200

    Controller class update

--- src/app/Controller.java
+++ src/app/Controller.java
@@ -1,7 +1,33 @@
 package app;
 
 
+import app.model.User;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class Controller {
 
 
+    @FXML
+    private TableView<User> table_view;
+
+    @FXML
+    private TableColumn<User, String> col_country;
+
+    @FXML
+    private TableColumn<User, String> col_custName;
+
+    @FXML
+    private TableColumn<User, String> col_buyerUnitNum;
+
+
+    private void initTable(){
+
+    }
+
+    private void initColumns(){
+
+
+    }
 }
--- src/app/model/User.java
+++ src/app/model/User.java
@@ -1,4 +1,36 @@
 package app.model;
 
 public class User {
+
+    String country, customerName, buyerUnitNumber;
+
+    public User(String country, String customerName, String buyerUnitNumber) {
+        this.country = country;
+        this.customerName = customerName;
+        this.buyerUnitNumber = buyerUnitNumber;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getBuyerUnitNumber() {
+        return buyerUnitNumber;
+    }
+
+    public void setBuyerUnitNumber(String buyerUnitNumber) {
+        this.buyerUnitNumber = buyerUnitNumber;
+    }
 }
